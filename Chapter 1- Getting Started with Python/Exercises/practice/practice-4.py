d52593981870fcf51e04029c27e2d07020347029
sana akhtar <sanaakhtarx1@gmail.com>
2023-10-04 09:51:45 +0400

    practice.py

---
+++ Chapter 1- Getting Started with Python/Exercises/practice/practice-4.py
@@ -0,0 +1,5 @@
+# compute the area of triangle
+b=int(input("Enter base"))
+h=int(input("Enter height"))
+Area=(b*h)/2
+print("Area=",Area)
---
+++ Chapter 1- Getting Started with Python/Exercises/practice/practice-5.py
@@ -0,0 +1,6 @@
+A="Amna"
+B="Ruqaia"
+C="Maria"
+D="Omama"
+E="Sidra"
+print(A,B,C,D,E)
